d8354c5ae9db00c62605a7df69c7a773b39698b9
GeorgeMcK <george@hookstream.com>
2025-03-07 07:26:04 -0800

    Guided Light App with Double Tap

--- GuidedLight/ViewController.swift
+++ GuidedLight/ViewController.swift
@@ -2,18 +2,42 @@
 //  ViewController.swift
 //  GuidedLight
 //
-//  Created by George McKinney on 3/6/25.
+//  Created by George McKinney on 2/6/25.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var LightState = false
+    
+    @IBOutlet weak var LightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UpdateUI()
+        view.backgroundColor = .black
+    }
+    
+    func UpdateUI(){
+        LightState.toggle()
+        view.backgroundColor = LightState ? .blue : .red
     }
 
+    @IBAction func TG(_ sender: UITapGestureRecognizer) {
+        print(sender)
+        LightState.toggle()
 
+        if(LightState == true){
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        print("pressed")
+        UpdateUI()
+    }
 }
 
